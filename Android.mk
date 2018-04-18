3a9cbfc99040e5a31e8235f2b8d18e069e2c6fec
CaoYongren <m18410261910@163.com>
2018-04-18 09:53:08 +0000

    Add Xposed to finish blocking association start.
    Signed-off-by: CaoYongren <m18410261910@163.com>

--- Android.mk
+++ Android.mk
@@ -22,7 +22,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src) \
     $(call all-renderscript-files-under, src) \
@@ -43,6 +42,8 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := android-support-v4:libs/android-support-v4.jar \
 
 include $(BUILD_PACKAGE)
 
